02ee511fc836aa7ab86a2fd72eeda057273f0fef
christinmendez <169026722+christinmendez@users.noreply.github.com>
2024-05-22 13:14:21 +0530

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,29 @@
+x=2255
+y=2255
+if x>y:
+   print("x is greaterthan y")
+else:
+    print("y is greater than x")
+a=555
+b=555
+if a>b:
+    print("a graterthan b")
+elif a==b:
+    print("x equal to b")
+else:
+    print("a less than b")
+xy=666
+if xy>10:
+    print("xy is above 10")
+    if xy>20:
+        print("xy is also above 20")
+    else:
+        print("xy is below 30")
+else:
+    print("xy is below 10")
+if x>y:
+    pass
+
+
+ 
+    
---
+++ loops.py
@@ -0,0 +1,19 @@
+# x=int(input("enter a value"))
+# while x<10:
+#     print(x)
+#     x+=1
+# y=1
+# while y<11:
+#     print(y)
+#     if y==5:
+#         break
+#     y+=1
+a=-1
+while a<10:
+    a+=1
+    if a==5:
+        continue
+    print(a)    
+    
+
+    
