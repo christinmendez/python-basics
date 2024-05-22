ac50fff8ec82682434fc930f6f6c5411006e5bb8
christinmendez <christinmendez@gmail.com>
2024-05-22 14:39:46 +0530

    conditiond

---
+++ tupple type/conditions/task conditions.py
@@ -0,0 +1,39 @@
+# x=int(input("enter a value"))
+# if x<0:
+#     print(" x is negative")
+# elif x>0:
+#     print("x is positive")
+# else:
+#     print("x is equal")
+# ab=int(input("enter a number"))
+# if ab%2==0:
+#     print("it is even number")
+# else:
+#     print("it is odd number")
+# x=int(input("enter a value"))
+# y=int(input("enter a second value"))
+# z=input("enter a oprator")
+# if z=="+":
+#     sum=x+y
+#     print(sum)
+# elif z=="-":
+#     dif=x-y
+#     print(dif)
+# elif z=="/":
+#     div=x/y
+#     print(div)
+# elif z=="*":
+#     mult=x*y
+#     print(mult)
+# else :
+#     print("invalid operator")
+# no=int(input("enter a number"))
+# fact=1
+# while(no>1):
+#     fact=fact*no
+#     no=no-1
+# print(fact)
+
+
+
+    
