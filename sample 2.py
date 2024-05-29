3bfe9c4bf9777fea6045408a3934ca55adae7aa3
christinmendez <169026722+christinmendez@users.noreply.github.com>
2024-05-29 13:22:36 +0530

    Add files via upload

---
+++ sample 2.py
@@ -0,0 +1,22 @@
+import os
+# x=open("sample.txt","r")
+# # print(x.read())
+# # print(x.read(6))
+# print(x.readline())
+# print(x.close())
+# x.close()
+# y=open("sample.txt","a")
+# y.write("the man is the man")
+# y.close()
+# y=open("sample.txt","r")
+# print(y.read())
+# y.close()
+# a=open("sample.txt","w")
+# a.write("there was a monke in kolothupadi")
+# a.close()
+# # b=open("sampleofsample","x")
+# os.remove("sampleofsample")
+os.rmdir("empty")
+
+
+
---
+++ tasksample.py
@@ -0,0 +1,15 @@
+# a=open("hrllodear.txt","x")
+# y=open("hrllodear.txt","w")
+# y.write("hello world")
+# y.close()
+# x=open("hrllodear.txt","a")
+# x.write("men will always be men")
+# z=open("hrllodear.txt","w")
+# z.write("give respect take respect")
+b=open("hrllodear.txt","r")
+# print(b.read())
+# print(b.read(3))
+print(b.readline())
+
+
+
